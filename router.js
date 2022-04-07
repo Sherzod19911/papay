69761f62bf77d5999f267e33a7547d2b20a1a91b
martin <martin@comets.kr>
2022-04-08 00:59:06 +0900

    feat: member controllerlarni qurish

--- router.js
+++ router.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const memberController = require("./contollers/memberController");
 
-router.get("/", function (req, res) {
-  res.send("home sahifadasiz");
-});
+// memberga dahldor routerlar
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
 
+// boshqa routerlar
 router.get("/menu", (req, res) => {
   res.send("Menu sahifadasiz");
 });
-
 router.get("/community", (req, res) => {
   res.send("Jamiyat sahifadasiz");
 });
