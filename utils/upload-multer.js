1951f303f6cadd3487bb8dc1c33e9c6b7e8ab7d4
Sherzod19911 <sherzod.baratov.1991@mail.ru>
2023-10-25 22:15:26 +0900

    feat:Product schema modelini hosil qilsih

--- utils/upload-multer.js
+++ utils/upload-multer.js
@@ -4,16 +4,16 @@ const uuid = require("uuid");
 
 /**MULTER IMAGE UPLOADER */
 function  getTargetImageStorage(address) {
-    return multer.diskStorage(destination=function (req, file, cb) {
-        cb(null, `./uploads/${address}`);
+    return multer.diskStorage({destination:function (req, file, cb) {
+        cb(null, `./uploads/products`);
     },
-    filename = function (req, file,cb) {
+    filename: function (req, file,cb) {
       console.log(file);
       const extention = path.parse(file.originalname).ext;
       const random_name = uuid.v4() +extention;
       cb(null, random_name);
     }
-  );
+  });
 }
 
 const makeUploader = (address) => {
